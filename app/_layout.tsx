3b341ee3d0e4a5971d9a7103635f822d6545c9be
Catalin <geaigelsaflu@gmail.com>
2024-08-02 13:49:05 +0300

    Finished tab bar design and functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,92 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack, Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import React, { useState } from "react";
 
 export default function RootLayout() {
+  let [fontsLoaded] = useFonts({
+    MontserratLight: require("../assets/fonts/Montserrat-Light.ttf"),
+    Montserrat: require("../assets/fonts/Montserrat-Regular.ttf"),
+    MontserratSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    MontserratBold: require("../assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  const [indexFocused, setIndexFocused] = useState(true);
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            indexFocused ? 
+            <MaterialIcons name="add-shopping-cart" size={39} color="black" /> : 
+            <MaterialIcons name="add-shopping-cart" size={32} color="darkgray" />
+          ),
+          tabBarItemStyle: indexFocused
+            ? styles.tabBarItem
+            : styles.tabBarItemFocused,
+          headerTitle: "Adaugă o nouă cheltuială",
+          headerTitleAlign: "center",
+          headerTitleStyle: styles.title,
+        }}
+        listeners={{
+          tabPress: () => {
+            setIndexFocused(true);
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="browse"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            !indexFocused ?
+            <MaterialIcons name="history" size={42} color="black" /> : 
+            <MaterialIcons name="history" size={34} color="gray" />
+          ),
+          tabBarIconStyle: !indexFocused
+            ? styles.tabBarItem
+            : styles.tabBarItemFocused,
+          tabBarItemStyle: styles.tabBarItem,
+          headerTitle: "Vizualizare cheltuieli",
+          headerTitleAlign: "center",
+          headerTitleStyle: styles.title,
+        }}
+        listeners={{
+          tabPress: () => {
+            setIndexFocused(false);
+          },
+        }}
+      />
+    </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "MontserratSemiBold",
+    fontSize: 22,
+  },
+  header: {},
+  tabBar: {
+    height: 80,
+    paddingTop: 15,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    borderTopColor: "gray",
+    margin: 100,
+    marginBottom: 25,
+  },
+  tabBarIcon: {},
+  tabBarItem: {},
+  tabBarItemFocused: {},
+});
--- app/browse.tsx
+++ app/browse.tsx
@@ -2,7 +2,7 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import React from "react";
 
-export default function Index() {
+export default function Browse() {
   let [fontsLoaded] = useFonts({
     MontserratLight: require("../assets/fonts/Montserrat-Light.ttf"),
     Montserrat: require("../assets/fonts/Montserrat-Regular.ttf"),
@@ -19,12 +19,26 @@ export default function Index() {
         backgroundColor: "white",
       }}
     >
-      
+       
     </View>
-      
   );
 }
 
 const styles = StyleSheet.create({
-  
+  title: {
+    fontFamily: "Montserrat",
+    fontSize: 20,
+  },
+  header: {},
+  tabBar: {
+    height: 80,
+    borderRadius: 0,
+    borderWidth: 0,
+    borderTopWidth: 1,
+    borderTopColor: "lightgray",
+    paddingTop: 10,
+  },
+  tabBarIcon: {},
+  tabBarItem: {},
+  tabBarItemFocused: {},
 });
